a7414205fcc961164a0e04f0147b12984b3e6f35
LetMeFly666 <814114971@qq.com>
2022-02-23 16:28:11 +0800

    只需要执行脚本“pushhere **”即可自动完成twine的打包、pypi的发布、release的发布

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@
 Author: LetMeFly
 Date: 2022-01-25 17:46:23
 LastEditors: LetMeFly
-LastEditTime: 2022-02-23 16:23:15
+LastEditTime: 2022-02-23 16:25:11
 '''
-version = '0.0.19'
+version = '0.0.20'
 
 if __name__ == "__main__":
     from setuptools import find_packages
