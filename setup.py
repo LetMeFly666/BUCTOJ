37a2f23280aeb07f1b7dead14378618849e12638
LetMeFly666 <814114971@qq.com>
2022-03-09 17:15:29 +0800

    try to build again with the private project And try to use the version 0.0.19

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@
 Author: LetMeFly
 Date: 2022-01-25 17:46:23
 LastEditors: LetMeFly
-LastEditTime: 2022-02-24 13:47:00
+LastEditTime: 2022-03-09 17:15:06
 '''
-version = '0.0.21'
+version = '0.0.19'
 
 if __name__ == "__main__":
     from setuptools import find_packages
