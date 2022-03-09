ec1c823aefd771de3f917fb28ad7997c97f81a92
LetMeFly666 <814114971@qq.com>
2022-03-09 23:06:41 +0800

    小修复：代码忘记换行导致rst文档中不再是代码块

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@
 Author: LetMeFly
 Date: 2022-01-25 17:46:23
 LastEditors: LetMeFly
-LastEditTime: 2022-03-09 23:03:29
+LastEditTime: 2022-03-09 23:06:39
 '''
-version = '0.1.0'
+version = '0.1.1'
 
 if __name__ == "__main__":
     from setuptools import find_packages
